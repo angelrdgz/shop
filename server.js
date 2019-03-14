21b6f0025a439e51e9f7ce26a5d5a96d78688bf1
Ángel García <agarcia@grainchain.io>
2019-03-14 14:10:41 -0600

    again

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/shop-diana'));
+app.use(express.static(__dirname + '/dist/shop'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/shop-diana/index.html'));
+res.sendFile(path.join(__dirname+'/dist/shop/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
